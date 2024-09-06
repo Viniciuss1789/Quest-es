673b39bd654113859ec1ed110bc035a0061a3840
Vinicius <161778225+Viniciuss1789@users.noreply.github.com>
2024-09-05 21:10:42 -0300

    Add files via upload

--- js/script8.js
+++ js/script8.js
@@ -1,6 +1,6 @@
-var preco = parseFloat(prompt("Digite o preco do produto : "))
-var percentualDesconto = parseFloat(prompt("Digite o percentual : "))
-var desconto = (preco * percentualDesconto)/100;
-var precoFinal = preco - desconto;
+let preco = parseFloat(prompt("Digite o preco do produto : "))
+let percentualDesconto = parseFloat(prompt("Digite o percentual : "))
+let desconto = (preco * percentualDesconto)/100;
+let precoFinal = preco - desconto;
 alert("O valor do desconto é : " + desconto)
-alert("O preco final com desconto é : " + precoFinal)
+alert("O preco final com desconto é : " + preco)
